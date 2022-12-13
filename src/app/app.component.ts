ecf156cca3f1dc82ae8a49ecbc3e1596b97d95c4
Cernovschi Vasilica <vasilica.cernovschi@student.usv.ro>
2022-12-13 10:33:18 +0200

    feature: add a put and a delete request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,35 +8,40 @@ import { UserService } from './service/user.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
+  id: number = 3;
   private user: User = {
-    'id': 1,
-    'name': 'Vasilica Cernovschi',
+    'id': 2,
+    'name': 'Alina Cernovschi',
     'username': 'Vasilica1',
-    'email': 'vasilica.cernovschi@april.biz',
-    'address': {
-      'street': 'Kulas Light',
-      'suite': 'Apt. 556',
-      'city': 'Suceava',
-      'zipcode': '92998-3874',
-      'geo': {
-        'lat': '-37.3159',
-        'lng': '81.1496'
-      }
-    },
-    'phone': '1-770-736-8031 x56442',
-    'website': 'hildegard.org',
-    'company': {
-      'name': 'MindIt',
-      'catchPhrase': 'Multi-layered client-server neural-net',
-      'bs': 'harness real-time e-markets'
-    }
+    // 'email': 'vasilica.cernovschi@april.biz',
+    // 'address': {
+    //   'street': 'Kulas Light',
+    //   'suite': 'Apt. 556',
+    //   'city': 'Moara',
+    //   'zipcode': '92998-3874',
+    //   'geo': {
+    //     'lat': '-37.3159',
+    //     'lng': '81.1496'
+    //   }
+    // },
+    // 'phone': '1-770-736-8031 x56442',
+    // 'website': 'alina.org',
+    // 'company': {
+    //   'name': 'MindIt',
+    //   'catchPhrase': 'Multi-layered client-server neural-net',
+    //   'bs': 'harness real-time e-markets'
+    // }
   }
   
   constructor(private userService: UserService) {}
 
   ngOnInit() {
     this.onGetUsers();
-    this.onGetUser();
+    //this.onGetUser();
+    //this.onCreateUser();
+    //this.onUpdateUser();
+    //this.onPatchUser();
+    this.onDeleteUser();
   }
 
   onGetUsers() {
@@ -52,6 +57,38 @@ export class AppComponent implements OnInit{
       (response) => console.log(response),
       (error: any) => console.log(error),
       () => console.log('Done getting users')
+    )
+  }
+
+  onCreateUser() {
+    this.userService.createUser(this.user).subscribe(
+      (response) => console.log(response),
+      (error: any) => console.log(error),
+      () => console.log('Done creating users')
+    )
+  }
+
+  onUpdateUser() {
+    this.userService.updateUser(this.user).subscribe(
+      (response) => console.log(response),
+      (error: any) => console.log(error),
+      () => console.log('Done updating users')
+    )
+  }
+
+  onPatchUser() {
+    this.userService.patchUser(this.user).subscribe(
+      (response) => console.log(response),
+      (error: any) => console.log(error),
+      () => console.log('Done patching users')
+    )
+  }
+
+  onDeleteUser() {
+    this.userService.deleteUser(this.id).subscribe(
+      (response) => console.log(response),
+      (error: any) => console.log(error),
+      () => console.log('Done delete users')
     )
   }
 }
--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../interfaces/user';
+import { HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,8 @@ export class UserService {
   constructor(private http: HttpClient) { }
 
   getUsers(): Observable<User[]> {
-    return this.http.get<User[]>('assets/users.json');
+    const myHeaders = new HttpHeaders({'myheader': 'headervalue'});
+    return this.http.get<User[]>('assets/users.json', {headers: myHeaders});
   }
 
   getUser(): Observable<User> {
@@ -20,5 +22,17 @@ export class UserService {
 
   createUser(user: User): Observable<User> {
     return this.http.post<User>('https://jsonplaceholder.typicode.com/users', user);
+  }
+
+  updateUser(user: User): Observable<User> {
+    return this.http.put<User>(`https://jsonplaceholder.typicode.com/users/${user.id}`, user);
+  }
+
+  patchUser(user: User): Observable<User> {
+    return this.http.patch<User>(`https://jsonplaceholder.typicode.com/users/${user.id}`, user);
+  }
+
+  deleteUser(id: number): Observable<void> {
+    return this.http.delete<void>(`https://jsonplaceholder.typicode.com/users/${id}`);
   }
 }
